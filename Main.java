755cab240b7cb6f1d9395c401754825a0fec3261
orinachmani1 <73288898+orinachmani1@users.noreply.github.com>
2022-05-17 21:24:41 +0300

    additon of function and table for "preparingGamesSchedule"

--- Main.java
+++ Main.java
@@ -7,19 +7,28 @@ import Domain.UserController;
 public class Main {
     public static void main(String[] args) throws Exception {
         UserApplication up = new UserApplication();
-        String[] str = new String[2];
-        str[0] = "RefName1";
-        str[1] = "RefName2";
-        up.RefreePlacement("2020-2021", "La Liga", str);
-        up.LoginUser("David", "12345");
-        up.LoginUser("David", "12345");
-        up.LogOut();
-        up.LoginUser("Rep", "pass"); // works
-        up.RefreePlacement("2020-2021", "La Liga", str);
-        up.RefreePlacement("2020-2021", "La Lga", str);
-        str[0] = "RefName3";
-        up.RefreePlacement("2020-2021", "La Liga", str);
-        up.RefreePlacement("2021-2022", "La Liga", str);
+        //String[] str = new String[2];
+        // str[0] = "RefName1";
+        // str[1] = "RefName2";
+        // up.RefreePlacement("2020-2021", "La Liga", str);
+        // up.LoginUser("David", "12345");
+        // up.LoginUser("David", "12345");
+        // up.LogOut();
+        // up.LoginUser("Rep", "pass"); // works
+        // up.RefreePlacement("2020-2021", "La Liga", str);
+        // up.RefreePlacement("2020-2021", "La Lga", str);
+        // str[0] = "RefName3";
+        // up.RefreePlacement("2020-2021", "La Liga", str);
+        // up.RefreePlacement("2021-2022", "La Liga", str);
+        // up.AddTeam("FC Barcelona", 1,4);
+        // up.AddTeam("Sevilla FC", 1,4);
+        // up.AddTeam("Real Sociedad", 1,4);
+        // up.AddTeam("Athletic Bilbao", 1,4);
+        // up.AddTeam("Valencia CF", 1,4);
+        // up.AddTeam("Osasuna", 1,4);
+        // up.AddTeam("Espanyol", 1,4);
+        //up.createGameSchedule();
+        //up.preparingGamesSchedule(1, 4, , )
         /**
          * UserApplication up = new UserApplication();
          * Scanner scanner = new Scanner(System.in);
