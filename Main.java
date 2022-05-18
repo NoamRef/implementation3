86789e0d3a17112344687a20afe1a62785823da7
orinachmani1 <73288898+orinachmani1@users.noreply.github.com>
2022-05-18 20:54:26 +0300

    - add preparingGamesSchedule (functionality & table) - add teams to Teams table  - add GamesSchedule for La Liga

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import Service.UserApplication;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import Domain.UserController;
@@ -7,19 +8,21 @@ import Domain.UserController;
 public class Main {
     public static void main(String[] args) throws Exception {
         UserApplication up = new UserApplication();
-        //String[] str = new String[2];
-        // str[0] = "RefName1";
-        // str[1] = "RefName2";
-        // up.RefreePlacement("2020-2021", "La Liga", str);
-        // up.LoginUser("David", "12345");
-        // up.LoginUser("David", "12345");
-        // up.LogOut();
-        // up.LoginUser("Rep", "pass"); // works
-        // up.RefreePlacement("2020-2021", "La Liga", str);
-        // up.RefreePlacement("2020-2021", "La Lga", str);
-        // str[0] = "RefName3";
-        // up.RefreePlacement("2020-2021", "La Liga", str);
-        // up.RefreePlacement("2021-2022", "La Liga", str);
+        String[] str = new String[2];
+        str[0] = "RefName1";
+        str[1] = "RefName2";
+        up.RefreePlacement("2020-2021", "La Liga", str);
+        up.LoginUser("David", "12345");
+        up.LoginUser("David", "12345");
+        up.LogOut();
+        up.LoginUser("Rep", "pass"); // works
+        up.RefreePlacement("2020-2021", "La Liga", str);
+        up.RefreePlacement("2020-2021", "La Lga", str);
+        str[0] = "RefName3";
+        up.RefreePlacement("2020-2021", "La Liga", str);
+        up.RefreePlacement("2021-2022", "La Liga", str);
+        
+        //Use case 3
         // up.AddTeam("FC Barcelona", 1,4);
         // up.AddTeam("Sevilla FC", 1,4);
         // up.AddTeam("Real Sociedad", 1,4);
@@ -27,8 +30,17 @@ public class Main {
         // up.AddTeam("Valencia CF", 1,4);
         // up.AddTeam("Osasuna", 1,4);
         // up.AddTeam("Espanyol", 1,4);
-        //up.createGameSchedule();
-        //up.preparingGamesSchedule(1, 4, , )
+        // up.createGameSchedule();
+        // ArrayList<String> levels = new ArrayList<>();
+        // levels.add("Quarter-Final");
+        // levels.add("Semi-Final");
+        // levels.add("Final");
+        // ArrayList<String> dates= new ArrayList<>();
+        // dates.add("01/01/2023 21:00");
+        // dates.add("08/01/2023 21:00");
+        // dates.add("15/01/2023 21:00");
+        // up.preparingGamesSchedule("La Liga","2020-2021","1","4",levels, dates);
+        
         /**
          * UserApplication up = new UserApplication();
          * Scanner scanner = new Scanner(System.in);
